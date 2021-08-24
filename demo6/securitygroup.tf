24ca93d61984d746de3686bd70f1d190ae22f11c
DhanushGitHub <dhanush.kr93@gmail.com>
2021-08-24 17:05:51 +0530

    added securitygroup.tf

---
+++ demo6/securitygroup.tf
@@ -0,0 +1,19 @@
+data "aws_ip_ranges" "ip" {
+  regions = ["eu-west-1","eu-central-1"]
+  services = ["ec2"]
+}
+
+resource "aws_security_group" "from_europe_Dhanush" {
+  name = "from_europe_Dhanush"
+
+  ingress {
+      from_port= "443"
+      to_port= "443"
+      protocol = "tcp"
+      cidr_blocks= data.aws_ip_ranges.ip.cidr_blocks
+  }
+  tags = {
+    CreateDate = data.aws_ip_ranges.ip.create_date
+    #SyncToken = data.aws_ip_ranges.ip.sync_token
+  }
+}
