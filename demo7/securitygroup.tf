ab720556c5601576d4ccbb213f24e9cf7830a61b
DhanushGitHub <dhanush.kr93@gmail.com>
2021-08-25 12:39:05 +0530

    Changed securitygroup to add new Ingress for EC2 and NGINX if needed

--- demo7/securitygroup.tf
+++ demo7/securitygroup.tf
@@ -13,7 +13,15 @@ resource "aws_security_group" "allow-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0", "148.64.5.119/32"]
+    cidr_blocks = ["148.64.5.119/32"] #Adding my local IP
+  }
+
+//If you do not add this rule, you can not reach the NGINX  or any web server you need on EC2
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
